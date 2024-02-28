271c1c274e1cdc03caa14abbfb9b044c10124ada
meeklife <forsonbaah@gmail.com>
2024-02-28 16:07:46 +0000

    Setup authentication on endpoints

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -128,5 +128,12 @@ AUTH_USER_MODEL = 'todo_app.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = True
