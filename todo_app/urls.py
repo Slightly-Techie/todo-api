04555773cef4ea57ab8395e8a4cad520a1c8f790
meeklife <forsonbaah@gmail.com>
2024-03-22 15:06:58 +0000

    changed the index route to api/get

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list, name='index'),
+    path('api/get', views.todo_list, name='index'),
     path('api/create', views.create_todo, name='create_todo'),
     path('api/view/<int:todo_id>', views.view_todo, name='view todo'),
     path('api/update/<int:todo_id>', views.update_todo, name='updating_todo'),
